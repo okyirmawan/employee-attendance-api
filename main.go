6b0f871250b68a76ea0ce0b1e38f2777836a028e
okyirmawan <oky.irmawan@gmail.com>
2024-01-04 17:13:56 +0700

    Add env variable for HOST

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/okyirmawan/employee-attendance-api/docs"
 	routes2 "github.com/okyirmawan/employee-attendance-api/routes"
-	"os"
+	"github.com/okyirmawan/employee-attendance-api/utils"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	environment := utils.Getenv("ENVIRONMENT", "development")
+
+	if environment == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			logrus.Fatal("Error loading .env file")
+		}
+	}
+
 	// programmatically set swagger info
 	docs.SwaggerInfo.Title = "Swagger Employee Attendance API"
 	docs.SwaggerInfo.Description = "This is a employee attendance API."
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "localhost:" + getPort()
+	docs.SwaggerInfo.Host = utils.Getenv("HOST", "localhost:8080")
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	routes := routes2.Init()
-	routes.Logger.Fatal(routes.Start(":" + getPort()))
-}
-
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	return port
+	routes.Logger.Fatal(routes.Start(":" + utils.Getenv("PORT", "8080")))
 }
